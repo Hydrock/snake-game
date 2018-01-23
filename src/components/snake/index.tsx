5dbb075e345012d12403f4c403c44d1625733a43
Alex Vechkanov <hydrock74@gmail.com>
2018-01-23 04:27:59 +0300

    add cnake vector

--- src/components/snake/index.tsx
+++ src/components/snake/index.tsx
@@ -18,24 +18,24 @@ interface SnakeProps {
 
 class Snake extends React.Component<SnakeProps> {
 
-  getStartPosition = () => {
-    const startHorizontal = this.props.size.width / 2;
-    const startVertical = this.props.size.height / 2;
-    return {startHorizontal, startVertical};
-  }
+  // getStartPosition = () => {
+  //   const startHorizontal = this.props.size.width / 2;
+  //   const startVertical = this.props.size.height / 2;
+  //   return {startHorizontal, startVertical};
+  // }
 
-  calculateCellNumberStart = () => {
-    // const coordinates = this.getStartPosition();
-    const coordinates = {
-      startHorizontal: this.props.snake.positionX,
-      startVertical: this.props.snake.positionY,
-    };
-    const cellsInFullRows = (coordinates.startVertical - 1) * this.props.size.width;
-    const cellNumber = cellsInFullRows + coordinates.startHorizontal;
-    return cellNumber;
-  }
+  // calculateCellNumberStart = () => {
+  //   // const coordinates = this.getStartPosition();
+  //   const coordinates = {
+  //     startHorizontal: this.props.snake.positionX,
+  //     startVertical: this.props.snake.positionY,
+  //   };
+  //   const cellsInFullRows = (coordinates.startVertical - 1) * this.props.size.width;
+  //   const cellNumber = cellsInFullRows + coordinates.startHorizontal;
+  //   return cellNumber;
+  // }
 
-  calculateCellNumber = (x: number, y: number) => {
+  calculateCellNumber = (x: any, y: any) => {
     const coordinates = {
       startHorizontal: x,
       startVertical: y,
@@ -45,47 +45,52 @@ class Snake extends React.Component<SnakeProps> {
     return cellNumber;
   }
 
+  checkData = () => {
+    let snakeObj = this.props.snake;
+
+    console.log(snakeObj);
+    let positionX;
+    let positionY;
+
+    switch (snakeObj.horizontalVector) {
+      case c.SNAKE_HORIZONTAL_VECTOR_RIGHT:
+        positionX = snakeObj.positionX + 1;
+        if (positionX > this.props.size.width) { positionX = 1; }
+        break;
+      case c.SNAKE_HORIZONTAL_VECTOR_LEFT:
+        positionX = snakeObj.positionX - 1;
+        if (positionX < 1) { positionX = this.props.size.width; }
+        break;
+      default:
+        break;
+    }
+    
+    switch (snakeObj.verticalVector) {
+      case c.SNAKE_VERTICAL_VECTOR_UP:
+        positionY = snakeObj.positionY - 1;
+        if (positionY < 1) { positionY = this.props.size.height; }
+        break;
+      case c.SNAKE_VERTICAL_VECTOR_DOWN:
+        positionY = snakeObj.positionY + 1;
+        if (positionY > this.props.size.height) { positionY = 1; }
+        break;
+      default:
+        break;
+    }
+
+    setSnakeData({
+      number: this.calculateCellNumber(positionX, positionY),
+      positionX: positionX,
+      positionY: positionY
+    });
+  }
+
   startSnake = () => {
-    let snake = this.props.snake;
-
-    let checkData = (snakeObj: any) => {
-      console.log(snakeObj);
-      let positionX;
-      let positionY;
-
-      switch (snakeObj.horizontalVector) {
-        case c.SNAKE_HORIZONTAL_VECTOR_RIGHT:
-          positionX = snakeObj.positionX + 1;
-          break;
-        case c.SNAKE_HORIZONTAL_VECTOR_LEFT:
-          positionX = snakeObj.positionX - 1;
-          break;
-        default:
-          break;
-      }
-      
-      switch (snakeObj.verticalVector) {
-        case c.SNAKE_VERTICAL_VECTOR_UP:
-          positionY = snakeObj.positionY - 1;
-          break;
-        case c.SNAKE_VERTICAL_VECTOR_DOWN:
-          positionY = snakeObj.positionY + 1;
-          break;
-        default:
-          break;
-      }
-
-      console.log()
-
-      setSnakeData({
-        number: this.calculateCellNumber(positionX, positionY),
-        positionX: positionX,
-        positionY: positionY
-      });
-    };
+    // let snake = this.props.snake;
+    let checkData = this.checkData;
 
     function step() {
-      setTimeout(function () { checkData(snake); step(); }, 1000);
+      setTimeout(function () { checkData(); step(); }, 1000);
     }
 
     step();
